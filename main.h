f469845d76ccaad29b685c758dd740e441b519a1
strongvillecode <107783160+strongvillecode@users.noreply.github.com>
2022-10-21 19:26:47 +0100

    Update main.h

--- main.h
+++ main.h
@@ -12,8 +12,7 @@
 #define F_PLUS 2
 #define F_ZERO 4
 #define F_HASH 8
-
-define F_SPACE 16
+#define F_SPACE 16
 
 /* SIZES */
 #define S_LONG 2
@@ -114,6 +113,5 @@ int is_digit(char);
 
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
-
 
 #endif /* MAIN_H */
