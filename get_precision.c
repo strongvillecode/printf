d334f7ca8851c2f1a17ce649d2c876d5434841df
strongvillecode <107783160+strongvillecode@users.noreply.github.com>
2022-10-22 04:50:01 +0100

    Get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -39,3 +39,33 @@ int get_precision(const char *format, int *i, va_list list)
 
 	return (precision);
 }
+
+
+/**
+ * get_precision - gets the precision from the format string
+ * @s: the format string
+ * @params: the parameters struct
+ * @ap: the argument pointer
+ * Return: new pointer
+*/
+
+char *get_precision(char *s, params_t *params, va_list ap)
+{
+	int d = 0;
+
+	if (*s != '.')
+		return (s);
+	s++;
+	if (*s == '*')
+	{
+		d = va_arg(ap, int);
+		s++;
+	}
+	else
+	{
+		while (_isdigit(*s))
+			d = d * 10 + (*s++ - '0');
+	}
+	params->precision = d;
+	return (s);
+}
